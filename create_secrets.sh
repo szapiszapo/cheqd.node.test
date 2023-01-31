b4a254dbfbe8585441e77de216d0a32f792db6b1
szapiszapo <szapiszapo@vaipoa.com>
2023-01-31 23:13:23 +0100

    refactored create_secrets.sh 	modified:   create_secrets.sh

--- create_secrets.sh
+++ create_secrets.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-while read line; do
+prompt_password() {
+  #Keep prompting for passwords until they match
+  while true; do
+    read -p "Enter a password for $key:" password
+    echo
+    read -p "Verify password:" verify
+    if [ "$password" == "$verify" ]; then
+      # Passwords match, return the password
+      return
+    else
+      # Passwords do not match, print a message and continue loop
+      echo "Passwords do not match. Please try again."
+    fi
+  done
+}
+
+for line in $(cat /opt/$PROJECT_NAME/secrets.env); do
   key=$(echo $line | cut -d '=' -f 1)
   value=$(echo $line | cut -d '=' -f 2)
 
-  if [ "$value" = "prompt" ]; then
-    # Keep prompting for passwords until they match
-    while true; do
-      echo -n "Enter a password for $key: "
-      read password
-      echo -n "Verify password: "
-      read verify
-      if [ "$password" = "$verify" ]; then
-        # Passwords match, exit loop
-        break
-      else
-        # Passwords do not match, print a message and continue loop
-        echo "Passwords do not match. Please try again."
-      fi
-    done
-
+  if [ "$value" == "prompt" ]; then
+    password=$(prompt_password 0)
     value=$(echo -n $password | openssl passwd -apr1)
     docker secret update "$key" - <<< "$value"
-    if [ "$key" = "TRAEFIK_HASHED_PASSWORD" ]; then
+    if [ "$key" == "TRAEFIK_HASHED_PASSWORD" ]; then
       echo "This password will be used to access $key and should be stored."
-      echo "Press Enter to acknowledge..."
+      e cho "Press Enter to acknowledge..."
       read
     fi
-  elif [ "$value" = "encrypt" ]; then
+  elif [ "$value" == "encrypt" ]; then
     value=$(openssl rand -base64 32)
     docker secret update "$key" - <<< "$value"
   fi
-done < /opt/$PROJECT_NAME/secrets.env
+done
+
 
 
